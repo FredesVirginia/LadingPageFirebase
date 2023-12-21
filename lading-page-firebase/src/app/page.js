88365500552a6d4feda507b4c4a2b000494e7721
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-12-21 18:34:55 +0000

    ffre

--- lading-page-firebase/src/app/page.js
+++ lading-page-firebase/src/app/page.js
@@ -2,18 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="  bg-gradient-to-b from-color1 to-color2 ">
-       <div className=" pt-10 px-40   h-[450px]">
-     <div className='flex  space-x-56'>
+    <div className="  ">
+       <div className=" pt-10 px-56   h-[450px]">
+     <div className='flex  space-x-52'>
       <form>
         <h1 className='font-bold text-xl text-white'>Jonh Clein Developer</h1>
-        <h2 className='text-white mb-20'> Service Firebase for Android</h2>
+        <h2 className='text-white mb-36'> Service Firebase for Android</h2>
 
         <h2 className='font-bold text-xl text-white' > This is Best Program</h2>
          <div className='w-20 my-5 h-2 bg-gradient-to-b from-color3 to-pink-600'>
 
          </div>
-        <h3 className=' text-white mb-5'> Thank for Buyght</h3>
+        <h3 className=' text-white mb-9'> Thank for Buyght</h3>
           <div className='flex flex-col space-y-6'>
           <input 
          className='border-gray-400 w-80'
@@ -32,19 +32,16 @@ export default function Home() {
       </form>
 
       <div>
-        <div className='border-solid border-white border-4 w-80 h-[390px] bg-gradient-to-b from-color3 to-pink-600'>
-        jhb
-        </div>
+      <video width="320" height="240" controls autoPlay>
+      <source src="./Img/v3.mp4" type="video/mp4" />
+      Tu navegador no soporta el elemento de video.
+    </video>
       </div>
      </div>
      
     </div>
-    <div className="    h-[140px]">
-      Imagen Repetida
-    </div>
-    <footer className='bg-gradient-to-b from-color3 to-pink-600 h-[170px]'>
-      gfbg
-     </footer>
+   
+    
     </div>
   )
 }
