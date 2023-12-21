a3987be9472776996e1a040b6f3db074da9f6d3d
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-12-21 17:58:16 +0000

    fefer

--- lading-page-firebase/src/app/page.js
+++ lading-page-firebase/src/app/page.js
@@ -2,14 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=" pt-10 px-40 bg-gradient-to-b from-color1 to-color2 h-[450px]">
-     <div>
+    <div className="  bg-gradient-to-b from-color1 to-color2 ">
+       <div className=" pt-10 px-40   h-[450px]">
+     <div className='flex  space-x-56'>
       <form>
         <h1 className='font-bold text-xl text-white'>Jonh Clein Developer</h1>
         <h2 className='text-white mb-20'> Service Firebase for Android</h2>
 
         <h2 className='font-bold text-xl text-white' > This is Best Program</h2>
-        <h3 className=' text-white'> Thank for Buyght</h3>
+         <div className='w-20 my-5 h-2 bg-gradient-to-b from-color3 to-pink-600'>
+
+         </div>
+        <h3 className=' text-white mb-5'> Thank for Buyght</h3>
           <div className='flex flex-col space-y-6'>
           <input 
          className='border-gray-400 w-80'
@@ -26,13 +30,19 @@ export default function Home() {
         </button>
           </div>
       </form>
-      <div>
-        <div>
 
+      <div>
+        <div className='border-solid border-white border-4 w-80 h-[390px] bg-gradient-to-b from-color3 to-pink-600'>
+        jhb
         </div>
       </div>
      </div>
-     <footer className=''>
+     
+    </div>
+    <div className="    h-[140px]">
+      Imagen Repetida
+    </div>
+    <footer className='bg-gradient-to-b from-color3 to-pink-600 h-[170px]'>
       gfbg
      </footer>
     </div>
